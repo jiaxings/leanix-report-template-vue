fb095755c08a37dabdd29c0c0c68ee87a783c7fb
Paulo dos Santos <paulo.santos@leanix.net>
2018-03-22 10:58:00 +0100

    minor change in router configuration

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import FactSheetTypes from '@/components/FactSheetTypes'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', name: 'FactSheetTypes', component: FactSheetTypes }
   ]
 })
